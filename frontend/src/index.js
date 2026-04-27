454c8fb932e6b3626f8ccc5d38d456a4eab3d4eb
anilsg08-dev <anilsg08@gmail.com>
2026-04-27 17:32:00 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// Create root element and render App
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
