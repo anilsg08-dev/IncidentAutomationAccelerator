858cd10c4df4f9805fe2ef7efb471a9bc109e48e
anilsg08-dev <anilsg08@gmail.com>
2026-04-27 17:40:36 +0530

    Update rerun-etl.sql

--- scripts/rerun-etl.sql
+++ scripts/rerun-etl.sql
@@ -1,1 +1,25 @@
+/*
+------------------------------------------------------------
+ Script: rerun-etl.sql
+ Purpose: Re-run failed ETL job for automated incident resolution
+ Author: Incident Automation Accelerator
+------------------------------------------------------------
+*/
+
+-- Log start of ETL rerun
+PRINT 'Starting ETL job rerun at ' + CONVERT(VARCHAR, GETDATE(), 120);
+
+BEGIN TRY
+    -- Example: call stored procedure to re-run ETL job
+    EXEC usp_RunETLJob @JobID = 101;
+
+    -- Log success
+    PRINT 'ETL job rerun completed successfully at ' + CONVERT(VARCHAR, GETDATE(), 120);
+END TRY
+BEGIN CATCH
+    -- Log error details
+    PRINT 'ERROR: ETL job rerun failed at ' + CONVERT(VARCHAR, GETDATE(), 120);
+    PRINT 'Error Number: ' + CAST(ERROR_NUMBER() AS VARCHAR);
+    PRINT 'Error Message: ' + ERROR_MESSAGE();
+END CATCH;
 
